51381271b14691f1b2e5ef3f37e10a735b1194c8
unknown <sreldr03@louisvill.edu>
2021-02-19 11:19:54 -0500

    New

--- chess-server.js
+++ chess-server.js
@@ -1,13 +1,37 @@
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const mime = require('mime-types');
+
+const landing = "/chess.html";
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Test");
+    console.log(req.socket.localAddress + ":" + req.host + " => " + req.path)
+    if(req.path.startsWith("/api")){
+
+    }else{
+        let filename = "/front";
+        if(req.path == "/"){
+            filename += landing;
+        }else{
+            filename += req.path;
+        }
+        fs.createReadStream(filename).pipe(res);
+        fs.readFile(filename, (err, data) => {
+            if(err)throw err;
+            let type = mime.lookup(filename);
+            if(!type)throw new Error("No mime type for file");
+            res.writeHead(200, {"Content-Type": type});
+            res.write(data);
+        }).catch(err => {
+            res.writeHead(404);
+            if(typeof err == "object"){
+                res.write(err.message);
+            }else{
+                res.write(err);
+            }
+        });
+    }
+    res.end();
 });
-server.listen(process.env.PORT)
-// server.listen(port, hostname, () => {
-//     console.log(`Server running @ ${hostname}:${port}`);
-// })
+server.listen(process.env.PORT || 8080);
