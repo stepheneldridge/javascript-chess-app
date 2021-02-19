77a3af8571ce6db99edb794f1d1d4d268df278b6
unknown <sreldr03@louisvill.edu>
2021-02-19 04:02:20 -0500

    Setup

---
+++ chess-server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    console.log(req)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Test");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running @ ${hostname}:${port}`);
+})
