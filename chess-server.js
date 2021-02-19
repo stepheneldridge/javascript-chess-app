646fc53d95d609a407b208610d91f2f11281e203
unknown <sreldr03@louisvill.edu>
2021-02-19 17:43:46 -0500

    paths

--- chess-server.js
+++ chess-server.js
@@ -36,7 +36,7 @@ server.use(session({
     "cookie": {"secure": true}
 }));
 
-server.get("/", (req, res) => {
+server.get("/*", (req, res) => {
     if(DEBUG_MODE)console.log(req.method + " => " + req.url);
     let url = new URL(req.url, `http://${req.headers.host}`);
     let filepath = "./front";
@@ -56,7 +56,10 @@ server.get("/", (req, res) => {
     });
 });
 
-
+server.get("/api/*", (req, res) => {
+    req.write(200, "api wins");
+    req.end();
+});
 // const server = http.createServer((req, res) => {
 //     if(DEBUG_MODE)console.log(req.method + " => " + req.url);
 //     let url = new URL(req.url, `http://${req.headers.host}`);
